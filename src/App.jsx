747c5438b6c512d12cfa9c2364a1fa4bff7f10b9
mhmahdi97 <mhmahdi97@gmail.com>
2023-12-19 00:12:12 +0600

    created course data for displaying in the card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 
 
 function App() {
-    
+
+  const courseData = [
+    {
+      name: 'This is a one line title',
+      description: 'Two line paragraph Lorem ipsum dolor sit amet, consectetur',
+      price: 1050,
+      discount: 300,
+      discountedPrice: 800
+    },
+    {
+      name: 'This is a one line title',
+      description: 'Two line paragraph Lorem ipsum dolor sit amet, consectetur',
+      price: 1050,
+      discount: null,
+      discountedPrice: null
+    },
+    {
+      name: 'This is a one line title',
+      description: 'Two line paragraph Lorem ipsum dolor sit amet, consectetur',
+      price: null,
+      discount: null,
+      discountedPrice: null
+    },
+  ]
 
   return (
     <>
-      
+      <h1 className="">Discount: {courseData[0].discountedPrice}</h1>
         
     </>
   )
